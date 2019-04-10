f07000f52eca42bf52b6e7f7d7c0e0254fdfa2de
Suraj Harale <surajh@winjit.com>
2019-04-10 11:59:20 +0200

    added new text field for search post

--- src/App.vue
+++ src/App.vue
@@ -2,32 +2,25 @@
   <v-app>
     <v-toolbar app>
       <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
+        <span>Posts</span>
       </v-toolbar-title>
       <v-spacer />
-      <v-btn
-        flat
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-      >
-        <span class="mr-2">Latest Release</span>
-      </v-btn>
+      <v-flex xs12 sm6 md3>
+        <v-text-field
+          label="Post Id"
+          append-icon="search"
+        ></v-text-field>
+        </v-flex>
     </v-toolbar>
-
     <v-content>
-      <HelloWorld />
     </v-content>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
-
 export default {
   name: 'App',
   components: {
-    HelloWorld
   },
   data () {
     return {
