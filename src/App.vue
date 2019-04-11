0a7e71bcc20b3fc13501ff426b078a4a0c4c7f1b
Suraj Harale <surajh@winjit.com>
2019-04-11 09:30:44 +0200

    Added load button

--- src/App.vue
+++ src/App.vue
@@ -5,12 +5,34 @@
         <span>Posts</span>
       </v-toolbar-title>
       <v-spacer />
-      <v-flex xs12 sm6 md3>
-        <v-text-field
-          label="Post Id"
-          append-icon="search"
-        ></v-text-field>
-        </v-flex>
+      <v-form 
+        xs12 
+        sm6 
+        md3 
+      > 
+        <v-container>
+          <v-layout>
+            <v-flex>
+              <v-text-field
+                v-model="searchText"
+                label="Post Id"
+                prepend-inner-icon="search"
+                class="flex-Padding"
+              />           
+            </v-flex>
+            <v-flex
+              class="flex-btn-padding "
+            > 
+              <v-btn
+                small 
+                color="primary"
+              >
+                Load
+              </v-btn>
+            </v-flex>
+          </v-layout>
+        </v-container>
+      </v-form>
     </v-toolbar>
     <v-content>
     </v-content>
@@ -24,8 +46,24 @@ export default {
   },
   data () {
     return {
-      //
+      searchText: '',
+      posts: [],
     }
-  }
+  },
 }
 </script>
+<style>
+  .flex-Padding {
+    padding-top: 1px !important;
+    padding-left: 3px !important;
+    padding-right: 3px !important;
+    padding-bottom: 3px !important;
+  }
+  .flex-btn-padding {
+    padding-top: 1px !important;
+    padding-left: 3px !important;
+    padding-right: 3px !important;
+    padding-bottom: 3px !important;
+  }
+</style>
+
