138b97b40e7763fc4b192398ded4b4755c9fbbbc
Suraj Harale <surajh@winjit.com>
2019-04-11 13:09:09 +0200

    Added load post  method to load post on button click

--- src/App.vue
+++ src/App.vue
@@ -5,41 +5,63 @@
         <span>Posts</span>
       </v-toolbar-title>
       <v-spacer />
-      <v-form 
-        xs12 
-        sm6 
-        md3 
-      > 
-        <v-container>
+    </v-toolbar>
+    <v-content>
+      <v-card>
+        <v-container
+          fluid
+          grid-list-lg
+        >
           <v-layout>
             <v-flex>
               <v-text-field
                 v-model="searchText"
                 label="Post Id"
-                prepend-inner-icon="search"
-                class="flex-Padding"
+                single-line
+                outline
               />           
             </v-flex>
-            <v-flex
-              class="flex-btn-padding "
-            > 
+            <v-flex> 
               <v-btn
-                small 
+                large 
                 color="primary"
+                @click="loadPost"
               >
                 Load
               </v-btn>
             </v-flex>
           </v-layout>
+          <v-layout 
+            v-for="(post, index) in posts"
+            :key="index"
+          >
+            <v-flex xs12>
+              <v-card>
+                <v-card-title primary-title>
+                  <div>
+                    <div>
+                      Post Id: {{ post.id }}
+                    </div>
+                    <div class="headline">
+                      {{ post.title }}
+                    </div>
+                    <span>
+                      {{ post.body }}
+                    </span>
+                  </div>
+                </v-card-title>
+              </v-card>
+            </v-flex>
+          </v-layout>
         </v-container>
-      </v-form>
-    </v-toolbar>
-    <v-content>
+      </v-card>
     </v-content>
   </v-app>
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
   name: 'App',
   components: {
@@ -50,17 +72,34 @@ export default {
       posts: [],
     }
   },
+  mounted () {
+    var vm =this;
+    axios.get("https://jsonplaceholder.typicode.com/posts")
+    .then(function (response) {
+        vm.posts=response.data;
+      })
+  },
+  methods: {
+    loadPost () {
+      var vm =this;
+      axios.get("https://jsonplaceholder.typicode.com/posts/" + this.searchText)
+      .then(function (response) {
+        vm.posts = [];
+        vm.posts.push(response.data);
+      })
+    },
+  },
 }
 </script>
 <style>
   .flex-Padding {
-    padding-top: 1px !important;
+    padding-top: 18px !important;
     padding-left: 3px !important;
     padding-right: 3px !important;
     padding-bottom: 3px !important;
   }
   .flex-btn-padding {
-    padding-top: 1px !important;
+    padding-top: 18px !important;
     padding-left: 3px !important;
     padding-right: 3px !important;
     padding-bottom: 3px !important;
